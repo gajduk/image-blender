399de2a4d7642e6c320e1296ddb69c4664f176d9
Andrej <gajduk@users.noreply.github.com>
2021-11-02 10:49:25 +0100

    Formating and readme

--- main.js
+++ main.js
@@ -17,22 +17,29 @@ const {
     size = 100,
 } = argv;
 
+const imageRequestOptions = {
+    width,
+    height,
+    color,
+    size
+}
+
 fetchAndBlendImages();
 
 async function fetchAndBlendImages() {
     try {
-        let images = await fetchImages(getImageRequests());
-        let blendedImage = await blendImagesHorizontally(images, width);
+        let images = await fetchImages(getImageRequests([greeting, who], imageRequestOptions));
+        let blendedImage = await blendImagesHorizontally(images, imageRequestOptions.width);
         await writeFile(fileOut, blendedImage, 'binary');
         console.log("The file was saved!");
-    } catch ( err ) {
+    } catch (err) {
         console.log(err);
     }
 }
 
-function getImageRequests(words, width, height, color, size) {
+function getImageRequests(words, opts) {
     return words.map(word => ({
-        url: `${baseUrl}${word}?width=${width}&height=${height}&color${color}&s=${size}`,
+        url: `${baseUrl}${word}?width=${opts.width}&height=${opts.height}&color${opts.color}&s=${opts.size}`,
         method: 'get',
         responseType: 'arraybuffer'
     }));
@@ -40,26 +47,26 @@ function getImageRequests(words, width, height, color, size) {
 
 async function fetchImages(imageRequests) {
     let imageResponses = await Promise.all(imageRequests.map(axios.request));
-    console.log('Received responses with status: ' + imageResponses.map(response => response.status));
+    console.log('Received responses with status: ', imageResponses.map(response => response.status));
     return imageResponses.map(response => response.data);
 }
 
 function blendImagesHorizontally(imageArray, width) {
     return new Promise((resolve, reject) => {
         blend(imageArray.map((image, idx) => ({
-                buffer: image,
-                x: idx*width,
-                y: 0,
-            })), {
-                width: width * imageArray.length,
-                height: height,
-                format: 'jpeg',
-            }, (err, data) => {
-                if ( err ) {
-                    reject(err);
-                } else {
-                    resolve(data); 
-                }
-            });
+            buffer: image,
+            x: idx * width,
+            y: 0,
+        })), {
+            width: width * imageArray.length,
+            height: height,
+            format: 'jpeg',
+        }, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
         });
+    });
 }
--- readme.md
+++ readme.md
@@ -6,8 +6,13 @@ Refactor my application to improve, harden and modernize the code. Aim for:
 - Maintainability
 - Performance
 
-Things that can be improved:
+Things that were improved:
 
 * simplicity - use async/await instead of callbacks, avoid callback hell  
 * maintainability - extract functions, e.g. for blending, creating requests   
 * performance - send two requests at once rather then one after the other  
+
+Additional things that could be improved with more time:
+
+* add unit and component tests
+* modularize the code to separate concerns
